b83c1c65b048b39ce4475da22920ee7101608294
wei ching wang <andyw330@gmail.com>
2017-03-20 16:35:48 +0800

    add triangle boundary testing

--- triangle/triangle.cc
+++ triangle/triangle.cc
@@ -1,6 +1,6 @@
 #include "triangle.h"
 
-// Returns triangle type.
+// Returns triangle type. (upper bound 200)
 const char* triangle(int a, int b, int c) {
   return "NotATriangle";
 }
--- triangle/triangle_unittest.cc
+++ triangle/triangle_unittest.cc
@@ -2,7 +2,50 @@
 #include "gtest/gtest.h"
 
 
-// Tests triangle().
-TEST(TriangleTest, JustTesting) {
-  EXPECT_STREQ("NotATriangle", triangle(1, 2, 3));
+// Tests triangle(). (upper bound 200)
+
+// Boundary Value Testing
+// Normal
+TEST(TriangleTest, NormalBoundary) {
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 1));
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 2));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 199));
+  EXPECT_STREQ("NotATriangle", triangle(100, 100, 200));
+  EXPECT_STREQ("Isosceles", triangle(100, 1, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 2, 100));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 199, 100));
+  EXPECT_STREQ("NotATriangle", triangle(100, 200, 100));
+  EXPECT_STREQ("Isosceles", triangle(1, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(2, 100, 100));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(199, 100, 100));
+  EXPECT_STREQ("NotATriangle", triangle(200, 100, 100));
+}
+// Robust
+TEST(TriangleTest, RobustBoundary) {
+  EXPECT_STREQ("NotATriangle", triangle(100, 100, 0));
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 1));
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 2));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 100, 199));
+  EXPECT_STREQ("NotATriangle", triangle(100, 100, 200));
+  EXPECT_STREQ("NotATriangle", triangle(100, 100, 201));
+
+  EXPECT_STREQ("NotATriangle", triangle(100, 0, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 1, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 2, 100));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(100, 199, 100));
+  EXPECT_STREQ("NotATriangle", triangle(100, 200, 100));
+  EXPECT_STREQ("NotATriangle", triangle(100, 201, 100));
+
+  EXPECT_STREQ("NotATriangle", triangle(0, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(1, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(2, 100, 100));
+  EXPECT_STREQ("Equilateral", triangle(100, 100, 100));
+  EXPECT_STREQ("Isosceles", triangle(199, 100, 100));
+  EXPECT_STREQ("NotATriangle", triangle(200, 100, 100));
+  EXPECT_STREQ("NotATriangle", triangle(201, 100, 100));
 }
