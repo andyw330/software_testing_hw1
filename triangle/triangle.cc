69288f33b7ab894ffdac97a6d4f41a9e899a9e6c
wei ching wang <andyw330@gmail.com>
2017-03-20 16:51:54 +0800

    triangle bug fix, test pass

--- triangle/triangle.cc
+++ triangle/triangle.cc
@@ -2,7 +2,8 @@
 
 // Returns triangle type. (upper bound 200)
 const char* triangle(int a, int b, int c) {
-  if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200) {
+  if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 ||
+      c > 200 || a + b <= c || b + c <= a || c + a <= b) {
     return "NotATriangle";
   }
   if (a == b && b == c)
