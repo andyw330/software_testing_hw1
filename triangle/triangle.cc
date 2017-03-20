36282e2ddbc3989c08a62f09f29c4a073e69a669
wei ching wang <andyw330@gmail.com>
2017-03-20 16:46:58 +0800

    write triangle code

--- triangle/triangle.cc
+++ triangle/triangle.cc
@@ -2,5 +2,13 @@
 
 // Returns triangle type. (upper bound 200)
 const char* triangle(int a, int b, int c) {
-  return "NotATriangle";
+  if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200) {
+    return "NotATriangle";
+  }
+  if (a == b && b == c)
+    return "Equilateral";
+  else if (a != b && a != c && b != c)
+    return "Scalene";
+  else
+    return "Isosceles";
 }
