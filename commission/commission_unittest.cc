01bcc86b50f59f860377580e61161ec69b7047fb
wei ching wang <andyw330@gmail.com>
2017-03-21 08:17:50 +0800

    add commission equivalence class testing

--- commission/commission_unittest.cc
+++ commission/commission_unittest.cc
@@ -2,3 +2,64 @@
 #include "gtest/gtest.h"
 
 // Tests commission().
+// Standard
+TEST(CommissionTest, StdBoundary) {
+  EXPECT_DOUBLE_EQ(10, commission(1, 1, 1));
+  EXPECT_DOUBLE_EQ(12.5, commission(1, 1, 2));
+  EXPECT_DOUBLE_EQ(13, commission(1, 2, 1));
+  EXPECT_DOUBLE_EQ(14.5, commission(2, 1, 1));
+  EXPECT_DOUBLE_EQ(50, commission(5, 5, 5));
+  EXPECT_DOUBLE_EQ(97.5, commission(10, 10, 9));
+  EXPECT_DOUBLE_EQ(97, commission(10, 9, 10));
+  EXPECT_DOUBLE_EQ(95.5, commission(9, 10, 10));
+  EXPECT_DOUBLE_EQ(100, commission(10, 10, 10));
+  EXPECT_DOUBLE_EQ(103.75, commission(10, 10, 11));
+  EXPECT_DOUBLE_EQ(104.5, commission(10, 11, 10));
+  EXPECT_DOUBLE_EQ(106.75, commission(11, 10, 10));
+  EXPECT_DOUBLE_EQ(160, commission(14, 14, 14));
+  EXPECT_DOUBLE_EQ(216.25, commission(18, 18, 17));
+  EXPECT_DOUBLE_EQ(215.5, commission(18, 17, 18));
+  EXPECT_DOUBLE_EQ(213.25, commission(17, 18, 18));
+  EXPECT_DOUBLE_EQ(220, commission(18, 18, 18));
+  EXPECT_DOUBLE_EQ(225, commission(18, 18, 19));
+  EXPECT_DOUBLE_EQ(226, commission(18, 19, 18));
+  EXPECT_DOUBLE_EQ(229, commission(19, 18, 18));
+  EXPECT_DOUBLE_EQ(820, commission(48, 48, 48));
+  EXPECT_DOUBLE_EQ(1415, commission(70, 80, 89));
+  EXPECT_DOUBLE_EQ(1414, commission(70, 79, 90));
+  EXPECT_DOUBLE_EQ(1411, commission(69, 80, 90));
+  EXPECT_DOUBLE_EQ(1420, commission(70, 80, 90));
+}
+// Special Value (Sales Boundary)
+TEST(CommissionTest, SpecialBoundary) {
+  EXPECT_DOUBLE_EQ(100.75, commission(10, 11, 9)); // sales = 1005
+  EXPECT_DOUBLE_EQ(219.25, commission(18, 17, 19)); // sales = 1795
+  EXPECT_DOUBLE_EQ(221, commission(18, 19, 17)); // sales = 1805
+}
+
+// Equivalence Class Testing
+// L1= {locks: 1 ≤ locks ≤ 70}
+// L2= {locks = –1} (occurs if locks = –1 is used to control input iteration)
+// S1 = {stocks: 1 ≤ stocks ≤ 80}
+// B1= {barrels: 1 ≤ barrels ≤ 90}
+// Weak Robust
+TEST(CommissionTest, CommissionWR) {
+  EXPECT_DOUBLE_EQ(100, commission(10, 10, 10));
+  EXPECT_EXIT(commission(-1, 40, 45), ::testing::ExitedWithCode(0), "Program terminates");
+  EXPECT_EXIT(commission(-2, 40, 45), ::testing::ExitedWithCode(1), "Value of locks not in the range");
+  EXPECT_EXIT(commission(71, 40, 45), ::testing::ExitedWithCode(1), "Value of locks not in the range");
+  EXPECT_EXIT(commission(35, -1, 45), ::testing::ExitedWithCode(1), "Value of stocks not in the range");
+  EXPECT_EXIT(commission(35, 81, -5), ::testing::ExitedWithCode(1), "Value of stocks not in the range");
+  EXPECT_EXIT(commission(35, 40, -1), ::testing::ExitedWithCode(1), "Value of barrels not in the range");
+  EXPECT_EXIT(commission(35, 40, 91), ::testing::ExitedWithCode(1), "Value of barrels not in the range");
+}
+// Strong Robust
+TEST(CommissionTest, CommissionSR) {
+  EXPECT_EXIT(commission(-2, 40, 45), ::testing::ExitedWithCode(1), "Value of locks not in the range");
+  EXPECT_EXIT(commission(35, -1, 45), ::testing::ExitedWithCode(1), "Value of stocks not in the range");
+  EXPECT_EXIT(commission(35, 40, -2), ::testing::ExitedWithCode(1), "Value of barrels not in the range");
+  EXPECT_EXIT(commission(-2, -1, 45), ::testing::ExitedWithCode(1), "Value of locks and stocks not in the range");
+  EXPECT_EXIT(commission(-2, 40, -1), ::testing::ExitedWithCode(1), "Value of locks and barrels not in the range");
+  EXPECT_EXIT(commission(35, -1, -1), ::testing::ExitedWithCode(1), "Value of stocks and barrels not in the range");
+  EXPECT_EXIT(commission(-2, -1, -1), ::testing::ExitedWithCode(1), "Value of locks and stocks and barrels not in the range");
+}
